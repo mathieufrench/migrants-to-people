578e675e08cc4ef6472c043208e6917ccd1ec91b
Mathieu French <git@mathieufrench.info>
2015-08-26 14:52:08 +0100

    Committing all files

---
+++ README.md
@@ -0,0 +1,4 @@
+Migrants to People
+=============
+
+Chrome extension that replaces the word 'Migrants' with 'people'.
---
+++ migrants-to-people/content_script.js
@@ -0,0 +1,43 @@
+walk(document.body);
+
+function walk(node)
+{
+	// I stole this function from here:
+	// http://is.gd/mwZp7E
+
+	var child, next;
+
+	switch ( node.nodeType )
+	{
+		case 1:  // Element
+		case 9:  // Document
+		case 11: // Document fragment
+			child = node.firstChild;
+			while ( child )
+			{
+				next = child.nextSibling;
+				walk(child);
+				child = next;
+			}
+			break;
+
+		case 3: // Text node
+			handleText(node);
+			break;
+	}
+}
+
+function handleText(textNode)
+{
+	var v = textNode.nodeValue;
+
+	v = v.replace(/\bMigrant\b/g, "Person");
+	v = v.replace(/\bMigrants\b/g, "People");
+	v = v.replace(/\bmigrant\b/g, "person");
+	v = v.replace(/\bmigrants\b/g, "people");
+
+
+	textNode.nodeValue = v;
+}
+
+
